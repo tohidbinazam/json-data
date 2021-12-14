23ab999b587b7912c843a662afe3c4aeb54620e1
tohidbinazam <tohidbinazamsunny1@gmail.com>
2021-12-14 22:05:27 +0600

    Uplode

---
+++ result.js
@@ -0,0 +1,64 @@
+const students = [
+    {
+        roll:1,
+        name:"shakil",
+        age:20,
+        fname:"shamajol",
+        bn:40,
+        en:45,
+        mat:50,
+        s:55,
+        ss:70,
+        rel:90
+  
+    },
+    {
+      roll:2,
+      name:"shakila",
+      age:10,
+      fname:"shamajol",
+      bn:50,
+      en:55,
+      mat:60,
+      s:55,
+      ss:33,
+      rel:80
+  
+  },
+  {
+      roll:3,
+      name:"karim",
+      age:25,
+      fname:"rafik",
+      bn:75,
+      en:55,
+      mat:55,
+      s:67,
+      ss:760,
+      rel:75
+  
+  },
+  {
+      roll:4,
+      name:"sahin",
+      age:30,
+      fname:"karim",
+      bn:40,
+      en:45,
+      mat:50,
+      s:75,
+      ss:70,
+      rel:70
+  
+  },
+  {
+      roll:5,
+      name:"shakil",
+      age:20,
+      fname:"shamajol",
+      bn:40,
+  }
+];
+
+let dev = JSON.stringify(students);
+console.log(dev);
